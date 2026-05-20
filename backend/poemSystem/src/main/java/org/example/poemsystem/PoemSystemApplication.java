aeb3b393874fad46d0e984fd43b1b0bff6a7a453
my <2876328254@qq.com>
2026-05-20 19:19:41 +0800

    初始化后端

---
+++ backend/poemSystem/src/main/java/org/example/poemsystem/PoemSystemApplication.java
@@ -0,0 +1,13 @@
+package org.example.poemsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoemSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PoemSystemApplication.class, args);
+    }
+
+}
---
+++ backend/poemSystem/src/test/java/org/example/poemsystem/PoemSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.poemsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoemSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
